03168ff1f150617a1f71d323911e1dba12a59f3a
Donald Brooks Arum <brooksd079@gmail.com>
2022-06-03 15:00:15 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Follow along with the examples here
+// Follow along with the examples heres
+
+function say(greeting, firstName) {
+    console.log("First Name: ", firstName);
+    console.log("Greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+}
+
+say("Goodbye", "Julio");
+say("Julio", "Hello");
+
+// Add function
+
+function add(x, y){
+    return x + y;
+}
+
+console.log(add(1, 2));
+console.log(add(9000, 80));
+
+function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+}
+
+console.log("Hello", "Liz");
+console.log(add(1, 2));
+
+function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+    console.log("I was called!");
+}
+
+console.log(say("howdy", "partner"));
